fc350ef03c4c44ff33f24e4a1b68ddcdc5d2c561
noebernigaud <noe.bernigaud@etu.unice.fr>
2022-06-18 08:21:47 +0200

    light intensity smooth transition

--- src/babylon/time.ts
+++ src/babylon/time.ts
@@ -1,4 +1,5 @@
 import { light } from "./main";
+import { Animation } from "babylonjs";
 
 export var hour: number
 
@@ -7,14 +8,19 @@ export function updateHour(hourInput: number) {
     hour = hourInput;
 
     //pleine nuit
-    if (hour == 23 || hour < 5) { light.intensity = 0.2; }
+    if (hour > 23 || hour < 5) { light.intensity = 0.2; }
 
     //levée du jour
-    else if (hour >= 5 && hour < 10) { light.intensity = (hour - 2) / 10; }
+    else if (hour >= 5 && hour <= 11) { smoothLightTransition(light.intensity, (hour - 3) / 10); }
 
     //journée
-    else if (hour >= 10 && hour < 17) { light.intensity = 0.8; }
+    else if (hour > 11 && hour < 17) { light.intensity = 0.8; }
 
     //couché du soleil
-    else if (hour >= 17 && hour < 23) { light.intensity = (25 - hour) / 10 }
+    else if (hour >= 17 && hour <= 23) { smoothLightTransition(light.intensity, (25 - hour) / 10); }
+}
+
+function smoothLightTransition(start: number, end: number) {
+    Animation.CreateAndStartAnimation("animMove", light, "intensity", 60, 60, start, end, Animation.ANIMATIONLOOPMODE_CONSTANT);
+
 }
