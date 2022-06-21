f78843c8e7a6d69ba7b3c6dfb8129c54fc331fc9
FissoreD <davide.fissore@etu.univ-cotedazur.fr>
2022-06-21 20:37:17 +0200

    Revert "Transpilage of ts in js"
    This reverts commit fc73f446d068ef2534fb0cd4b5b776f3285d9f65.

--- dist/clients/babylon/avatars/monster.js
+++
@@ -1,2 +0,0 @@
-import{Avatar}from"./avatar";export class Monster extends Avatar{constructor(scene,avatar_username,username,p){super(scene,avatar_username,username,p)}}
-//# sourceMappingURL=monster.js.map
--- dist/clients/babylon/avatars/player.js
+++
@@ -1,2 +0,0 @@
-import{Avatar}from"./avatar";export class Player extends Avatar{constructor(scene,avatar_username,username,p){super(scene,avatar_username,username,p)}}
-//# sourceMappingURL=player.js.map
