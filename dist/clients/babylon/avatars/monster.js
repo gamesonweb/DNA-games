fc73f446d068ef2534fb0cd4b5b776f3285d9f65
FissoreD <davide.fissore@etu.univ-cotedazur.fr>
2022-06-21 17:56:05 +0200

    Transpilage of ts in js

---
+++ dist/clients/babylon/avatars/monster.js
@@ -0,0 +1,2 @@
+import{Avatar}from"./avatar";export class Monster extends Avatar{constructor(scene,avatar_username,username,p){super(scene,avatar_username,username,p)}}
+//# sourceMappingURL=monster.js.map
---
+++ dist/clients/babylon/avatars/player.js
@@ -0,0 +1,2 @@
+import{Avatar}from"./avatar";export class Player extends Avatar{constructor(scene,avatar_username,username,p){super(scene,avatar_username,username,p)}}
+//# sourceMappingURL=player.js.map
