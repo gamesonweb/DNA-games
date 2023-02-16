40987407e617f3fe66a6237a5923b333fa4023fc
Davide Fissore <fisso9900@gmail.com>
2023-02-16 12:50:30 +0100

    Add setLocalPort to console

--- src/clients/connection/server_address.ts
+++ src/clients/connection/server_address.ts
@@ -5,5 +5,8 @@ export var SERVER_LINK = "ws://127.0.0.1:8080";
 if (windowExists()) {
   (window as any).setServerLink = function (link: string) {
     SERVER_LINK = link
+  };
+  (window as any).setLocalPort = function (port: string) {
+    SERVER_LINK = "ws://127.0.0.1:" + port
   }
 }
