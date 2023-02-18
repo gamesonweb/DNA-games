fac3015be086de7e29a6d9812de7ececce5fe545
Davide Fissore <fisso9900@gmail.com>
2023-02-18 15:53:19 +0100

    remove useless constructor of player

--- src/clients/babylon/avatars/classes/heroes/player.ts
+++ src/clients/babylon/avatars/classes/heroes/player.ts
@@ -1,13 +1,7 @@
-import { InstantiatedEntries, Mesh, Scene, Vector3 } from "babylonjs";
-import { intrinsicModelProperties } from "../models";
-import { createBasicShape } from "../../../others/tools";
+import { Vector3 } from "babylonjs";
 import { Avatar } from "../../avatarHeavy";
 
 export abstract class Player extends Avatar {
-
-    constructor(scene: Scene, avatar_username: string, p: intrinsicModelProperties) {
-        super(scene, avatar_username, p)
-    }
 
     take_damage(source: Vector3, amount: number, knockback_power = 1) {
         if (!this.takeHits) return
