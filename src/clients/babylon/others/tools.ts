2a568a1da3338ed1baa7486de34dacd0ee039681
Davide Fissore <fisso9900@gmail.com>
2023-02-23 16:57:28 +0100

    remove default from playerClassCreator

--- src/clients/babylon/others/tools.ts
+++ src/clients/babylon/others/tools.ts
@@ -206,8 +206,7 @@ export function playerClassCreator(playerClass: PLAYER_CLASSES_TYPE, username: s
             return new Assassin(scene, username)
         case "Healer":
             return new Healer(scene, username)
-        default: //throw new Error("Class player not implemented")
-            return new Mage(scene, username)
+        default: throw new Error("Class player not implemented")
     }
 }
 
