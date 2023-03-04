56ac138b20ef3ca16a4d39946d2c7a1163f3a8c7
noebernigaud <noe.bernigaud@etu.unice.fr>
2023-03-04 14:28:41 +0100

    glider's trails

--- src/clients/babylon/avatars/tools/glider.ts
+++ src/clients/babylon/avatars/tools/glider.ts
@@ -1,5 +1,5 @@
-import { Axis, InstancedMesh, Mesh, Ray, Vector3 } from "babylonjs";
-import { sphere1 } from "../../main";
+import { Axis, Color3, InstancedMesh, Mesh, MeshBuilder, Ray, StandardMaterial, TrailMesh, Vector3 } from "babylonjs";
+import { scene, sphere1 } from "../../main";
 import { SceneSoft } from "../../scene/sceneSoft";
 import { Player } from "../classes/heroes/player";
 import { ModelEnum } from "../classes/models";
@@ -10,10 +10,23 @@ export class Glider {
     gliderInstance: InstancedMesh | undefined
     mergedmodel = Mesh.MergeMeshes((ModelEnum.Glider.rootMesh?.clone() as Mesh).getChildMeshes() as Mesh[]) as Mesh;
     player: Player
+    trailHandlers: TrailHandler[]
 
     constructor(player: Player) {
         this.player = player
         this.gliderRay = new Ray(this.player.shape.position, new Vector3(0, -1, 0), 6);
+
+        //SET UP TRAIL MATERIAL
+        let trailMaterial = new StandardMaterial("sourceMat", scene)
+        trailMaterial.emissiveColor = trailMaterial.diffuseColor = Color3.White();
+        trailMaterial.specularColor = Color3.Black();
+        trailMaterial.alpha = 0.2;
+
+        //CREATE TRAIL HANDLERS
+        this.trailHandlers = [
+            new TrailHandler(trailMaterial, new Vector3(3.6, 1.1, -0.7)),
+            new TrailHandler(trailMaterial, new Vector3(-3.6, 1.1, -0.7))
+        ]
     }
 
     switchGlide() {
@@ -44,6 +57,7 @@ export class Glider {
 
     deployGlider(on: boolean) {
         if (on) {
+            //SPAWN GLIDER MODEL
             this.gliderInstance = this.mergedmodel.createInstance("glider_" + this.player.name)
             this.gliderInstance.isPickable = false
             this.gliderInstance.checkCollisions = false
@@ -51,8 +65,50 @@ export class Glider {
             this.gliderInstance.position = this.player.shape.position.add(new Vector3(0, 1.45, 0)).subtract(this.player.shape.getDirection(Axis.Z).normalize().scale(0.4))
             this.gliderInstance.computeWorldMatrix(true);
             this.player.shape.addChild(this.gliderInstance)
+            //DISPLAY TRAILS
+            this.trailHandlers.forEach(trailHandler => trailHandler.switchTrailOn(this.gliderInstance))
         } else {
-            if (this.gliderInstance) this.gliderInstance.dispose()
+            if (this.gliderInstance) {
+                //REMOVE GLIDER MODEL AND TRAILS
+                this.trailHandlers.forEach(trailHandler => trailHandler.switchTrailOff())
+                this.gliderInstance.dispose()
+            }
         }
+    }
+}
+
+class TrailHandler {
+    trailMaterial: StandardMaterial
+    trailEmitter: Mesh | undefined
+    trailMesh: TrailMesh | undefined
+    offset: Vector3
+
+    constructor(material: StandardMaterial, offset: Vector3) {
+        //SET UP TRAIL MATERIAL
+        this.trailMaterial = material
+        this.trailMaterial.emissiveColor = this.trailMaterial.diffuseColor = Color3.White();
+        this.trailMaterial.specularColor = Color3.Black();
+        this.trailMaterial.alpha = 0.2;
+        this.offset = offset
+    }
+
+    switchTrailOn(parent: InstancedMesh | undefined) {
+
+        this.trailEmitter = MeshBuilder.CreateBox("airstreak", { size: 0.05 });
+        if (parent) this.trailEmitter.parent = parent
+        this.trailEmitter.isVisible = false
+        this.trailEmitter.isPickable = false
+        this.trailEmitter.checkCollisions = false
+        this.trailEmitter.position = this.trailEmitter.position.add(this.offset)
+
+        this.trailMesh = new TrailMesh("airTrail", this.trailEmitter, scene, 0.01, 30);
+        this.trailMesh.material = this.trailMaterial
+
+        this.trailMesh.start(); //Starts the trailing mesh.
+    }
+
+    switchTrailOff() {
+        this.trailMesh?.dispose()
+        this.trailEmitter?.dispose()
     }
 }
